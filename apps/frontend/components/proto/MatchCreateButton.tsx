ff473ba8a1192bf83857a0f8aefc7ff779926ade
root <sushi.honda@gmail.com>
2026-04-12 14:54:27 +0800

    chore: rename fetch /api/rooms to /api/metches 2/n

--- apps/frontend/components/proto/MatchCreateButton.tsx
+++ apps/frontend/components/proto/MatchCreateButton.tsx
@@ -37,7 +37,7 @@ export function MatchCreateButton({
     setIsLoading(true);
 
     try {
-      const response = await fetch("/api/rooms", {
+      const response = await fetch("/api/matches", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
